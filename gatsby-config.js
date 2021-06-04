14c047f86d61ac8c28038f166fafc1125348bf2f
Kyle Schiller <kyleschiller@MacBook-Pro-2.local>
2021-06-04 12:45:55 -0400

    id

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        trackingId: "UA-127025029-1",
       },
     },
     {
